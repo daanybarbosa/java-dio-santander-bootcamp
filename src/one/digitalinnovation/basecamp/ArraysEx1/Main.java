9add4c12d4ed82c7ff2a5ae1a0e1c3bcac5bf0f6
Daniele Barbosa <daanybsilva@gmail.com>
2022-07-15 00:51:10 -0300

    Commit Aula 5 - Arrays

---
+++ src/one/digitalinnovation/basecamp/ArraysEx1/Main.java
@@ -0,0 +1,31 @@
+package one.digitalinnovation.basecamp.ArraysEx1;
+
+/**
+ * Ex 1 - Ordem Inversa
+ *
+ * Crie um vetor de 6 números inteiros e mostre-os na ordem inversa.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] vetor = {-5, -6, 15, 50, 8, 4}; //unidimensional
+
+        int count = 0;
+
+        System.out.print("Tamanho do vetor: " + vetor.length);
+        System.out.print("\nVetor original: ");
+
+        while(count <= vetor.length - 1){
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.print("\nVetor invertido: ");
+
+        for(int i = (vetor.length - 1); i >= 0; i--){
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
